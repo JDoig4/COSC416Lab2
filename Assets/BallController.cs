b2f546c3bb0bdd8ca217bda1c2e4786e808bb56a
JDoig4 <justindoig6@gmail.com>
2025-02-15 22:56:30 -0800

    Ball moves with player

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,6 +4,8 @@ using UnityEngine.Events;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
+
     private bool isBallLaunched;
     private Rigidbody ballRB;
     [SerializeField] private InputManager inputManager;
@@ -12,12 +14,19 @@ public class BallController : MonoBehaviour
     {
         ballRB = GetComponent<Rigidbody>();
         inputManager.OnSpacePressed.AddListener(LaunchBall);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+        ballRB.isKinematic = true;
+
     }
 
     private void LaunchBall()
     {
         if (isBallLaunched) return;
         isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
+        // sets the object to the outermost layer of the hierarchy
         ballRB.AddForce(transform.forward * force, ForceMode.Impulse);
     }
 }
