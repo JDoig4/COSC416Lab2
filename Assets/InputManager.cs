31838ecdfd85c8cd9eddf4c2440c3cefc0126b41
JDoig4 <justindoig6@gmail.com>
2025-02-15 23:56:45 -0800

    Added ball and pin models

--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -5,31 +5,29 @@ public class InputManager : MonoBehaviour
 {
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     public UnityEvent OnSpacePressed = new UnityEvent();
+    public UnityEvent OnResetPressed = new UnityEvent();
 
     private void Update()
     {
-        // Check if the space key is pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
             OnSpacePressed?.Invoke();
         }
 
-        // Initialize input vector
         Vector2 input = Vector2.zero;
-
-        // Check if the A key is pressed
         if (Input.GetKey(KeyCode.A))
         {
             input += Vector2.left;
         }
-
-        // Check if the D key is pressed
         if (Input.GetKey(KeyCode.D))
         {
             input += Vector2.right;
         }
-
-        // Invoke the OnMove event with the input vector
         OnMove?.Invoke(input);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OnResetPressed?.Invoke();
+        }
     }
 }
